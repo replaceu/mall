7ab654d9fc095d29981bd5b61bae51c9103453f0
aqiang9 <2903780002@qq.com>
2020-07-30 13:13:00 +0800

    重新整理项目结构

---
+++ gulimall-common/src/main/java/com/gulimall/common/product/dto/CategoryBrandRelationDto.java
@@ -0,0 +1,37 @@
+package com.gulimall.common.product.dto;
+
+import com.gulimall.common.valid.AddGroup;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+/**
+ * 品牌 与 分类 关联关系数据
+ * @author aqiang9  2020-07-30
+ */
+@Data
+public class CategoryBrandRelationDto {
+    private Long id;
+    /**
+     * 品牌id
+     */
+    @NotNull(message = "品牌id缺失" , groups = AddGroup.class)
+    @Positive(message = "品牌id必须大于0" ,groups = AddGroup.class)
+    private Long brandId;
+    /**
+     * 分类id
+     */
+    @NotNull(message = "分类id缺失", groups = AddGroup.class)
+    @Positive(message = "分类id必须大于0", groups = AddGroup.class)
+    private Long categoryId;
+    /**
+     *  品牌名
+     */
+    private String brandName;
+    /**
+     * 分类名
+     */
+    private String categoryName;
+
+}
