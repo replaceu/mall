997a99ccf489e738f2e9bd4d0bbb30f5808200c6
ReplaceU <carter.zhang@foreo.com>
2022-10-21 17:47:40 +0800

    commit

--- gulimall-pay/src/main/java/com/gulimall/pay/service/impl/RefundPayServiceImpl.java
+++ gulimall-pay/src/main/java/com/gulimall/pay/service/impl/RefundPayServiceImpl.java
@@ -1,13 +1,17 @@
 package com.gulimall.pay.service.impl;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.TypeReference;
 import com.gulimall.common.utils.R;
 import com.gulimall.pay.dto.PayRefundDto;
 import com.gulimall.pay.dto.PayRefundInfoDto;
 import com.gulimall.pay.feign.OrderFeignService;
 import com.gulimall.pay.service.RefundPayService;
+import com.gulimall.pay.vo.PayVo;
 
 @Service
 public class RefundPayServiceImpl implements RefundPayService {
@@ -16,8 +20,15 @@ public class RefundPayServiceImpl implements RefundPayService {
 
 	@Override
 	public PayRefundInfoDto createRefundByOrderId(PayRefundDto payRefundDto) {
-		R originalOrder = orderFeignService.getOrderPay(payRefundDto.getOrderId());
+		R originalOrderR = orderFeignService.getOrderPay(payRefundDto.getOrderId());
+		PayVo originalOrder = originalOrderR.getData(new TypeReference<PayVo>() {
+		});
 
+		PayRefundInfoDto insertRefund = new PayRefundInfoDto();
+		insertRefund.setOrderId(payRefundDto.getOrderId());
+		insertRefund.setReason(payRefundDto.getRefundReason());
+		insertRefund.setRefundFee(originalOrder.getTotalAmount());
+		insertRefund.setRefundId(UUID.randomUUID().toString());
 		return null;
 	}
 }
