835f67a1b721f157baf8c3f54944fab411f61b12
ReplaceU <carter.zhang@foreo.com>
2022-11-02 17:33:47 +0800

    commit

--- gulimall-integral/src/main/java/com/gulimall/integral/service/impl/IntegralFeeGradeServiceImpl.java
+++ gulimall-integral/src/main/java/com/gulimall/integral/service/impl/IntegralFeeGradeServiceImpl.java
@@ -105,6 +105,66 @@ public class IntegralFeeGradeServiceImpl implements IntegralFeeGradeService {
 
 	@Override
 	public void reduceUserFreeGrade(OrderTo orderTo) {
+		CommonResult<OrderTo> orderInfoTo = orderFeignService.getOrderStatus(orderTo.getOrderSn());
+		if (orderInfoTo.isOk()) {
+			OrderTo orderInfo = orderInfoTo.getData();
+			BigDecimal payAmount = orderInfo.getPayAmount();
+			//todo:查找配置的等级
+			List<IntegralMemberFreeGradeEntity> freeGradeList = integralMemberFreeGradeDao.selectList(new QueryWrapper<IntegralMemberFreeGradeEntity>());
+			//todo:查找用户之前的免费等级
+			IntegralUserFreeEntity userFreeGrade = integralUserFreeDao.selectOne(new LambdaQueryWrapper<IntegralUserFreeEntity>().eq(IntegralUserFreeEntity::getUserId, orderInfo.getMemberId().toString()));
+			//todo:算出用户降低至哪一个等级
+			String totalAmount = AmountUtil.subtractFenRetFen(userFreeGrade.getGradeAmount(), payAmount.toString());
+			IntegralMemberFreeGradeEntity afterGrade = null;
+			if (userFreeGrade != null) {
+				for (int i = freeGradeList.size() - 1; i >= 0; i--) {
+					if (AmountUtil.compareAmount(totalAmount, freeGradeList.get(i).getGreaterAmount()) >= 0 && AmountUtil.compareAmount(totalAmount, freeGradeList.get(i + 1).getGreaterAmount()) < 0) {
+						afterGrade = freeGradeList.get(i);
+					}
+					if (i == 0) {
+						if (AmountUtil.compareAmount(totalAmount, freeGradeList.get(i).getGreaterAmount()) >= 0) {
+							afterGrade = freeGradeList.get(i);
+						}
+					}
+				}
+			}
+			IntegralMemberFreeGradeEntity pinkGrade = freeGradeList.get(0);
+			IntegralMemberFreeGradeEntity goldGrade = freeGradeList.get(1);
+			Boolean isPinkOrGold = false;
+			if (afterGrade.getId().equals(pinkGrade.getId()) || afterGrade.getId().equals(goldGrade.getId())) {
+				isPinkOrGold = true;
+			}
+			//todo:去更新用户的等级
+			if (afterGrade != null) {
+				IntegralUserFreeEntity userFreeGradeInfo = new IntegralUserFreeEntity();
+				userFreeGradeInfo.setUserId(userFreeGrade.getUserId());
+				userFreeGradeInfo.setId(userFreeGrade.getId());
+				userFreeGradeInfo.setGradeAmount(totalAmount);
+				userFreeGradeInfo.setUpdateDate(new Date());
+				userFreeGradeInfo.setMemberFreeGradeId(afterGrade.getId());
+				userFreeGradeInfo.setEffectDate(new Date());
+				userFreeGradeInfo.setExpiredDate(isPinkOrGold ? IntegralMemberGradeConstant.RefValue.pinkExpiredDate : DateUtil.addDateWithDays(new Date(), Integer.valueOf(afterGrade.getLastDay())));
+				integralUserFreeDao.updateById(userFreeGradeInfo);
 
+				//todo:去插入用户的等级变更记录
+				IntegralUserGradeLogEntity toAddLog = new IntegralUserGradeLogEntity();
+				toAddLog.setChangeValue(afterGrade.getId());
+				toAddLog.setBeforeValue(userFreeGrade.getMemberFreeGradeId());
+				toAddLog.setCurrentValue(afterGrade.getId());
+				toAddLog.setCreateDate(new Date());
+				toAddLog.setUserId(userFreeGrade.getUserId());
+				toAddLog.setExpiredDate((isPinkOrGold ? IntegralMemberGradeConstant.RefValue.pinkExpiredDate : DateUtil.addDateWithDays(new Date(), Integer.valueOf(afterGrade.getLastDay()))));
+				toAddLog.setId(UUID.randomUUID().toString());
+				toAddLog.setEffectDate(new Date());
+				toAddLog.setIsDeleted(ICommonConstants.INT_TRUE);
+				toAddLog.setEffectDate(new Date());
+				toAddLog.setRefType(IntegralUserGradeLogConstants.RefType.refundSuccessDemote);
+				toAddLog.setReason(IntegralUserGradeLogConstants.Reason.refundSuccessDemote);
+				//refValue记录的是orderId
+				toAddLog.setRefValue(orderInfo.getOrderSn());
+				integralUserGradeLogDao.insert(toAddLog);
+			}
+
+		}
 	}
 }
